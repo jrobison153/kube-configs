6ee92a68c2c31e9fcde13c6cf9d69b438e6d95de
Justin Robison <jrobison151@gmail.com>
2018-11-14 06:10:34 -0500

    enabled ingress

--- istio/install-istio.sh
+++ istio/install-istio.sh
@@ -1,10 +1,12 @@
 # Install istio and tiller on a brand new cluster
-kubectl apply -f $ISTIO_HOME/install/kubernetes/helm/helm-service-account.yaml 
-helm init --service-account tiller 
+kubectl apply -f $ISTIO_HOME/install/kubernetes/helm/helm-service-account.yaml
+helm init --service-account tiller
 helm install $ISTIO_HOME/install/kubernetes/helm/istio \
 	--name istio \
 	--namespace istio-system \
 	--set tracing.enabled=true \
+	--set ingress.enabled=true \
+	--set ingress.service.type=NodePort \
 	--set gateways.istio-ingressgateway.type=NodePort \
 	--set gateways.istio-egressgateway.type=NodePort \
 	--set tracing.ingress.enabled=true \
